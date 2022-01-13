10f8349a366cc8ade17e9e7567e784a9dff74fcb
onesis <Onesis.olea@gmail.com>
2022-01-13 16:57:22 -0400

    Create table for greetings

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Greeting.create message: "Hello"
+Greeting.create message: "Hola"
+Greeting.create message: "Ciao"
+Greeting.create message: "Olá"
+Greeting.create message: "Hallo"

f312f6941b3d6b5d3b5bed5b52721bb0f2f48c27
onesis <Onesis.olea@gmail.com>
2022-01-16 22:23:31 -0400

    create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   root 'static#index'
+
+  namespace :api do
+    namespace :v1, defaults: {format:JSON} do
+      get 'greetings', to:'greetings#index'
+    end
+  end
 end

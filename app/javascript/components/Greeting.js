18c7ebe2a2021519b987f9d8ab4c0bfbf7044431
onesis <Onesis.olea@gmail.com>
2022-01-16 22:25:04 -0400

    create components

---
+++ app/javascript/components/Greeting.js
@@ -0,0 +1,23 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getGreeting } from "../redux/reducers/greetingReducer";
+
+const Greeting = () => {
+  const dispatch = useDispatch();
+  const { message } = useSelector((state) => state);
+
+  useEffect(() => {
+    dispatch(getGreeting());
+  }, []);
+
+  return (
+    <React.Fragment>
+      <div>
+        <h1>Greeting of the day</h1>
+        <p> {message}</p>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default Greeting;
